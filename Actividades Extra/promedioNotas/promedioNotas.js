c227c38273563839738f5cfd83140d4110dfc06d
Marto-kun <martinvozme456@gmail.com>
2025-01-14 21:59:22 -0500

    Pagina HTML para calcular el promedio entre 3 notas ingresadas

---
+++ Actividades Extra/promedioNotas/promedioNotas.js
@@ -0,0 +1,14 @@
+//Promedio con notas recuperadas
+calcularPromedioNotas = function () {
+  let n1;
+  let n2;
+  let n3;
+  let promedio;
+  let msg;
+  n1 = recuperarFlotante("txtN1");
+  n2 = recuperarFlotante("txtN2");
+  n3 = recuperarFlotante("txtN3");
+  promedio = calcularPromedio(n1, n2, n3).toFixed(2);
+  msg = "Tu promedio es: " + promedio;
+  cambiarTexto("lblPromedio", msg);
+};
---
+++ Actividades Extra/promedioNotas/servicioPromedioNotas.js
@@ -0,0 +1,6 @@
+//Calcular Promedio
+calcularPromedio = function (n1, n2, n3) {
+  let suma = n1 + n2 + n3;
+  let promedio = suma / 3;
+  return promedio;
+};
